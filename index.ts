95fcb39be946e8190f2f4e6892f171929da1cf4f
Taopol BERQUER <tao.berquer@gmail.com>
2022-07-04 11:33:13 +0200

    Ajout de mongodb

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { connect } from 'mongoose';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const mongo_uri = process.env.MONGO_URI || 'mongodb://localhost:27017/db';
+
+connect(mongo_uri);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
